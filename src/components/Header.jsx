8b6cdd3fb0bf2c1950148068447555d6235353c2
Juan Benega <jmbenega@gmail.com>
2024-12-04 06:20:50 -0300

    Trabajos sobre el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,37 @@
 import '../scss/components/_header.scss'
 
+import logo from '../assets/ainovaLogo.png'
+
+import Button from "./Button";
 
 
 const Header = () => {
 
+    //declaración de funciones
+  const handleClick = () => {
+    alert("Botón presionado!");
+  };
+
     return (
         <header className="header">
-            
+            <nav className='headerNav'>
+                <li className='headerMenu'>
+                    <p>
+                        Nosotros
+                    </p>
+                    <p>
+                        Servicios
+                    </p>
+                    <p>
+                        Portfolio
+                    </p>
+                </li>
+                <div className="headerLogo">
+                    <img src={logo} alt="AINOVA Logo" />
+                </div>
+                <p>Contacto</p>
+                <Button text="INICIO" onClick={handleClick}/>
+            </nav>
         </header>
     )
 
