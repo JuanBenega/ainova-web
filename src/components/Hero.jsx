bf6815297dec0830997e2189f8e4a5090333abee
Juan Benega <jmbenega@gmail.com>
2024-12-19 07:00:55 -0300

    modificaciones heroSection

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,13 @@ const Hero = () => {
   return (
     <div className="heroSection">
       <div className="heroContainer">
-        <h1 className="heroTitle">Innovación y crecimiento <strong>PARA TU EMPRESA</strong></h1>
-        <p className="heroText">Somos AINOVA una empresa de servicios tecnológicos en constante crecimiento, buscando siempre lo último en tecnologia para ayudarte.</p>
+        <p className="heroTitle">
+          Innovación y crecimiento <span className="titleRemark">PARA TU EMPRESA</span>
+        </p>
+        <p className="heroText">
+          Somos AINOVA una empresa de servicios tecnológicos en constante
+          crecimiento, buscando siempre lo último en tecnologia para ayudarte.
+        </p>
       </div>
     </div>
   );
