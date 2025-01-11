4e287b95ed86a4d5dc6dfa3f08d36998d7d08a4a
Juan Benega <jmbenega@gmail.com>
2025-01-11 08:34:06 -0300

    estilos en las cards de la seccion nosotros

--- src/components/Us.jsx
+++ src/components/Us.jsx
@@ -1,36 +1,42 @@
 import Card from "./Card";
 
+import iconTeam from "../assets/iconTeam.png";
+import iconGive from "../assets/iconGive.png";
+import iconPride from "../assets/iconPride.png";
+import iconClient from "../assets/iconClient.png";
+import iconReality from "../assets/iconReality.png";
+import iconDream from "../assets/iconDream.png";
 
 const Us = () => {
 
 const cardContent = [
   {
-    icon : "../assets/iconTeam.png",
+    icon : iconTeam,
     title: "Nos preocupamos por el equipo",
     text : "Trabajo en equipo y aprendizaje constante, un ambiente de crecimiento y reconocimiento continuo, nos caracteriza.",
   },
   {
-    icon : "../assets/iconGive",
+    icon : iconGive,
     title: "Damos lo mejor siempre",
     text : "Nos apasiona lo que hacemos. Cada proyecto es una muestra de nuestro compromiso y superación.",
   },
   {
-    icon : "../assets/iconPride.png",
+    icon : iconPride,
     title: "Orgullosos de lo que hacemos",
     text : "Desde la planificación hasta la entrega, cada etapa de nuestro trabajo está enfocada en la calidad.",
   },
   {
-    icon : "../assets/iconClient.png",
+    icon : iconClient,
     title: "Cuidamos a nuestros clientes",
     text : "Trabajamos con vos y te acompañamos para lograr tus objetivos, para eso queremos superar tus expectativas siempre.",
   },
   {
-    icon : "../assets/iconReality.png",
+    icon : iconReality,
     title: "Cambiamos realidades",
     text : "Buscamos soluciones creativas y disruptivas, impulsando ideas innovadoras para un futuro mejor.",
   },
   {
-    icon : "../assets/iconDream.png",
+    icon : iconDream,
     title: "Creemos en nuestros sueños",
     text : "Al igual que vos, somos conscientes de la realidad pero decidimos seguir soñando para poder darte siempre lo mejor.",
   }
@@ -38,6 +44,7 @@ const cardContent = [
   
   return (
     <div className="usSection">
+      <div className="usContainer">
       <section className="usTitle">
         <p className="badge">Nosotros</p>
         <h1 className="title">Nuestro equipo, nuestra historia</h1>
@@ -49,6 +56,7 @@ const cardContent = [
         ))}
       </section>
       <section className="usCarousel"></section>
+      </div>
     </div>
   );
 };
