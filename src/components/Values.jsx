be160dd3f11257025577135952d1dd29fbe5a3d4
Juan Benega <jmbenega@gmail.com>
2025-01-18 08:06:08 -0300

    modifique el disparo de la animacion para que inicie al llegar a la seccion

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,4 +1,6 @@
 import SectionTitle from "./SectionTitle";
+import React, { useEffect, useRef, useState } from "react";
+
 
 import valuesBack from "../assets/valuesBackground.png";
 import avatarInt from "../assets/avatarIntegridad.png";
@@ -11,6 +13,31 @@ import avatarAct from "../assets/avatarActitud.png";
 import avatarRes from "../assets/avatarResiliencia.png";
 
 const Values = () => {
+  const sectionRef = useRef(null); // Referencia al contenedor de la sección
+  const [isVisible, setIsVisible] = useState(false); // Estado para la visibilidad
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true); // Activa la animación
+        }
+      },
+      { threshold: 0.5 } // Se activa cuando el 20% de la sección es visible
+    );
+
+    if (sectionRef.current) {
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (sectionRef.current) {
+        observer.disconnect(); // Desactiva el observador
+      }
+    };
+  }, []);
+
+  
   return (
     <section className="section">
       <div className="valuesContainer">
@@ -19,13 +46,14 @@ const Values = () => {
           title="Lo que nos mueve"
           text="Nuestros valores son el corazón de nuestra empresa y eso lo hace un lugar especial"
         />
-        <div className="valuesPopup">
+        <div ref={sectionRef} className="valuesPopup">
           <img
+            //className={`valuesBack ${isVisible ? "animate" : ""}`}
             className="valuesBack"
             src={valuesBack}
             alt="Imagen de un celular"
           />
-          <div className="balloon balloon-1">
+          <div className={`balloon balloon-1 ${isVisible ? "animate" : ""}`}>
             <img src={avatarInt} alt="Imagen de integridad" />
             <div className="ballonText">
               Integridad
@@ -36,7 +64,7 @@ const Values = () => {
               </small>
             </div>
           </div>
-          <div className="balloon balloon-2">
+          <div className={`balloon balloon-2 ${isVisible ? "animate" : ""}`}>
             <img src={avatarExc} alt="Imagen de excelencia" />
             <div className="ballonText">
               Excelencia
@@ -47,7 +75,7 @@ const Values = () => {
               </small>
             </div>
           </div>
-          <div className="balloon balloon-3">
+          <div className={`balloon balloon-3 ${isVisible ? "animate" : ""}`}>
             <img src={avatarInn} alt="Imagen de innovación" />
             <div className="ballonText">
               Inonvación
@@ -58,7 +86,7 @@ const Values = () => {
               </small>
             </div>
           </div>
-          <div className="balloon balloon-4">
+          <div className={`balloon balloon-4 ${isVisible ? "animate" : ""}`}>
             <img src={avatarLid} alt="Imagen de liderazgo" />
             Liderazgo
             <div className="ballonText">
@@ -69,7 +97,7 @@ const Values = () => {
               </small>
             </div>
           </div>
-          <div className="balloon balloon-5">
+          <div className={`balloon balloon-5 ${isVisible ? "animate" : ""}`}>
             <img src={avatarCol} alt="Imagen de colaboración" />
             <div className="ballonText">
               Colaboración
@@ -80,7 +108,7 @@ const Values = () => {
               </small>
             </div>
           </div>
-          <div className="balloon balloon-6">
+          <div className={`balloon balloon-6 ${isVisible ? "animate" : ""}`}>
             <img src={avatarEmp} alt="Imagen de empatía" />
             <div className="ballonText">
               Empatía
@@ -91,7 +119,7 @@ const Values = () => {
               </small>
             </div>
           </div>
-          <div className="balloon balloon-7">
+          <div className={`balloon balloon-7 ${isVisible ? "animate" : ""}`}>
             <img src={avatarAct} alt="Imagen de actitud positiva" />
             <div className="ballonText">
               Actitud positiva
@@ -102,7 +130,7 @@ const Values = () => {
               </small>
             </div>
           </div>
-          <div className="balloon balloon-8">
+          <div className={`balloon balloon-8 ${isVisible ? "animate" : ""}`}>
             <img src={avatarRes} alt="Imagen de resiliencia" />
             <div className="ballonText">
               Resiliencia
