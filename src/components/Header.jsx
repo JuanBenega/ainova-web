b075b1d97abb880708dc586ce0bb80bc6bb33692
Juan Benega <jmbenega@gmail.com>
2025-03-31 06:11:13 -0300

    merge en develop de la rama fix, oculté el link a portafolio en el menú Toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
               <li><a href="#inicio">Inicio</a></li>
               <li><a href="#nosotros">Nosotros</a></li>
               <li><a href="#servicios">Servicios</a></li>
-              <li><a href="#portfolio">Portfolio</a></li>
+              {/* <li><a href="#portfolio">Portfolio</a></li> */}
               <li><a href="#contacto">Contacto</a></li>
             </ul>
 
